e67f0b2ed17b7cdc84448fedb546f90905abe912
AtaiVKC <victorimperium@hotmail.com>
2016-11-26 16:24:34 +0100

    programa de los colores

---
+++ 03_colores/colores.cpp
@@ -0,0 +1,54 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int amarillo,
+	    rojo,
+	    azul;
+
+		printf("\nVes el color amarillo? (no=0, si=1)\n");
+		scanf(" %i", &amarillo);
+	
+		printf("\nVes el color rojo? (no=0, si=1)\n");
+		scanf(" %i", &rojo);
+	
+		printf("\nVes el color azul? (no=0, si=1)\n");
+		scanf(" %i", &azul);
+		
+		if (amarillo == 0 && rojo == 0 && azul == 0)
+			
+			printf("\nEstás viendo el color negro\n\n");
+	
+		if (amarillo == 0 && rojo == 0 && azul == 1)
+		
+			printf("\nEstás viendo el color azul\n\n");
+	
+		if (amarillo == 0 && rojo == 1 && azul == 0)
+			
+			printf("\nEstás viendo el color rojo\n\n");
+	
+		if (amarillo == 0 && rojo == 1 && azul == 1)
+			
+			printf("\nEstás viendo el color morado\n\n");
+	
+		if (amarillo == 1 && rojo == 0 && azul == 0)
+			
+			printf("\nEstás viendo el color amarillo\n\n");
+	
+		if (amarillo == 1 && rojo == 0 && azul == 1)
+			
+			printf("\nEstás viendo el color verde\n\n");
+	
+		if (amarillo == 1 && rojo == 1 && azul == 0)
+			
+			printf("\nEstás viendo el color naranja\n\n");
+	
+		if (amarillo == 1 && rojo == 1 && azul == 1)
+			
+			printf("\nEstás viendo el color blanco\n\n");
+
+
+return EXIT_SUCCESS;
+}
