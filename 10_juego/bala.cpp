d37031337723ac164c02a2138e6883aa3a16eb85
AtaiVKC <victorimperium@hotmail.com>
2017-02-22 10:42:18 +0100

    actualización

--- 10_juego/bala.cpp
+++ 10_juego/bala.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DELTA 0.001
+#define DELTA 0.01
 #define MAX 10
 #define AcelX 0
 #define AcelY -10
@@ -13,6 +13,7 @@ int main(int argc, char *argv[]){
 
 	printf("\n Introduce la velocidad x e y:");
 	scanf(" %lf%lf", &velocidad_x, &velocidad_y);
+
 	for(int x = 0; x<MAX; x++){
 		printf(" %lf\n", velocidad_x += AcelX*DELTA);
 		printf(" %lf\n\n", velocidad_y += AcelY*DELTA);
